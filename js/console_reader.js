c749e528544bd5242123fa3dbc8b49bc718e1242
Jeremy <jeremyrwang@gmail.com>
2014-01-07 09:48:36 -0500

    Further work on console UI

--- js/console_reader.js
+++ js/console_reader.js
@@ -11,6 +11,12 @@ $(document).ready(function() {
         this.id = id;
         var feed_id = null;
         var done_reading = (starred == true);
+        
+        function strip(html) {
+           var tmp = $("<div>");
+           tmp.html(html);
+           return tmp.text();
+        }
 
         this.load = function($e) {
             $elem = $e;
@@ -24,10 +30,12 @@ $(document).ready(function() {
                 title.attr("target", "_blank");
                 title.addClass("article_title");
                 var content = $("<div>");
-                content.text(article.content);
+                content.text(strip(article.content));
                 content.addClass("article_content");
                 $elem.append(title);
                 $elem.append(content);
+                $elem.append($("<div>").append("&nbsp;"));
+                $("html, body").scrollTop($(document).height());
             }.bind(this));
         }
 
@@ -46,6 +54,10 @@ $(document).ready(function() {
         this.print = function(str) {
             var entry = $('<div>');
             entry.text(str);
+            if(str.length == 0) {
+                entry.html("&nbsp;");
+            }
+            entry.addClass("entry");
             $container.append(entry);
         }
 
@@ -62,24 +74,26 @@ $(document).ready(function() {
         function get_unread(feed) {
             selected_feed = feed;
             $.getJSON("list?feed=" + feed, function(data) {  // get user's unread reading list
-                display_articles(data, true);
+                load_articles(data, true);
             });
         }
 
         function get_starred() {
             $.getJSON("starred", function(data) {
                 selected_feed = 'Starred';
-                display_articles(data, true, true);
+                load_articles(data, true, true);
             });
         }
 
         function clear() {
             $("html, body").scrollTop(0);
-            content.empty();
-            loaded_articles = [];
+            $container.empty();
+            loaded_articles = loaded_articles.slice(viewing_index, loaded_articles.length);
+            console.log(loaded_articles);
+            viewing_index = 0;
         }
 
-        function display_articles(data, clear, starred) {
+        function load_articles(data, clear, starred) {
             var articles = data['articles'];
             
             for(var a = 0; a < articles.length; a++) {
@@ -95,7 +109,7 @@ $(document).ready(function() {
               // not paging starred as of now
             } else {
                 $.getJSON("list?feed=" + selected_feed + "&last=" + last_loaded, function(data) {  // get user's unread reading list
-                    display_articles(data, false);
+                    load_articles(data, false);
                     loading = false;
                 });
             }
@@ -104,21 +118,47 @@ $(document).ready(function() {
         // init
         $.getJSON("feeds", function(data) { // get user's feed
             var feeds = data["feeds"];
-            process_feeds(feeds);
-            update_unread(feeds);
-        });
+            for(var f = 0; f < feeds.length; f++) {
+                this.print("[" + feeds[f]["unread"] + "] " + feeds[f]["title"])
+            }
+            this.print("");
+            get_unread('all');
+        }.bind(this));
         
         function process(command) {
-            console.log("Command: " + command);
-        }
-
-        $(".cursor").keydown(function(event) {
-            var key = event.keyCode;
-            if(key == 32) { // Enter
-                var $source = $(event.source);
-                process($source.text());
-                $source.text("");
+            if(command == "") {
+                if(viewing_index >= loaded_articles.length) {
+                    this.print("No more articles.");
+                    this.print("");
+                    return;
+                }
+                loaded_articles[viewing_index].load($container);
+                loaded_articles[viewing_index].read();
+                viewing_index += 1;
+                if(viewing_index >= loaded_articles.length - 2) {
+                    load_more_articles(loaded_articles[loaded_articles.length - 1].id)
+                }
+            } else if(command == "clear") {
+                clear();
             }
-        }.bind(this));
+        };
+
+        this.register = function($input) {
+            $('body').keydown(function(event) {
+                $("html, body").scrollTop($(document).height());
+                var key = event.keyCode;
+                if(key == 13) { // Enter
+                    process.apply(this, [$input.text()]);
+                    $input.text("");
+                } else if(key >= 48 && key <= 90) { // a-z 0-9
+                    var chr = String.fromCharCode(event.keyCode).toLowerCase();
+                    $input.text($input.text() + chr);
+                } else if(key == 8) { // backspace
+                    var text = $input.text();
+                    $input.text(text.slice(0, text.length-1));
+                }
+                event.preventDefault();
+            }.bind(this));
+        }
     }
 }.bind(window));
